6e0f730ac9c24e715a1267ee9bd3ba9c734e7b3b
Tella Ramya Shree <67000854+Tella-Ramya-Shree@users.noreply.github.com>
2020-10-10 20:51:01 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,25 @@
+public class Employee{
+String name;
+int age;
+String city;
+public void display()
+                  {
+System.out.println("the name is:"+name);
+System.out.println("the age is "+age);
+System.out.println("the city is "+city);
+                  }
+public static void main(String args[])
+{
+Employee  e1=new Employee();
+e1.name="Ramya";
+e1.age=20;
+e1.city="Hyderabad";
+e1.display();
+
+Employee  e2=new Employee();
+e2.name="Ramya";
+e2.age=20;
+e2.city="banglore";
+e2.display();
+}
+}
